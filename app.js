fdd882524081ddc20f4f60bb16cf6232700f7de2
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-01-22 12:24:27 -0500

    Initial setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const connectDB = require('./config/db');
+const indexRouter = require('./routes/index')
+
+if(process.env.NODE_ENV !== 'production') {
+    dotenv.config({ path: './config/config.env' });
+}
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// Connect db
+connectDB()
+
+// Router
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
