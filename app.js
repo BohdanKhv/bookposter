040fd9853461d6aa2b5476d98902b7240032abf2
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-01-23 11:35:23 -0500

    Initial Author model

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 if(process.env.NODE_ENV !== 'production') {
     const dotenv = require('dotenv')
@@ -14,11 +16,14 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // Connect db
 connectDB()
 
 // Router
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
